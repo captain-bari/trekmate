756bd7efe9196f7a4fb5279e6959152d213245f8
captain-bari <barinder.turkey@gmail.com>
2021-09-12 00:52:47 +0530

    Initial build

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ const App = () => {
       ref={childRef}
       children={() => (
         <Switch>
-          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+          <AppRoute path="/" component={Home} layout={LayoutDefault} />
         </Switch>
       )} />
   );
