6a6eadbc92cfc3b39618f6ac54bac94425771536
captain-bari <barinder.turkey@gmail.com>
2021-09-12 00:37:00 +0530

    Initial build

---
+++ build/precache-manifest.5cc4114c142009ef7634441ba511b66c.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175371fe39e9754d1a93ac326573c5a2",
+    "url": "/trekmate/index.html"
+  },
+  {
+    "revision": "3dbe31c248727912dcf7",
+    "url": "/trekmate/static/css/2.a1667f21.chunk.css"
+  },
+  {
+    "revision": "c3a8a8222c4f8dca38ca",
+    "url": "/trekmate/static/css/main.628faf3c.chunk.css"
+  },
+  {
+    "revision": "3dbe31c248727912dcf7",
+    "url": "/trekmate/static/js/2.36434ea7.chunk.js"
+  },
+  {
+    "revision": "0ac79d53dda6ffa0c6325c90109848cb",
+    "url": "/trekmate/static/js/2.36434ea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a8a8222c4f8dca38ca",
+    "url": "/trekmate/static/js/main.1eb1696d.chunk.js"
+  },
+  {
+    "revision": "f1f3c8757beb5711eeb1",
+    "url": "/trekmate/static/js/runtime-main.0900a82f.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/trekmate/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/trekmate/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/trekmate/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/trekmate/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/trekmate/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/trekmate/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/trekmate/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/trekmate/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/trekmate/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/trekmate/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/trekmate/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/trekmate/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "9425960d08c171450679e8660a6a0c07",
+    "url": "/trekmate/static/media/features-split-image-01.9425960d.png"
+  },
+  {
+    "revision": "82ef21268cbbb69b091211d15ef1fbab",
+    "url": "/trekmate/static/media/features-split-image-02.82ef2126.png"
+  },
+  {
+    "revision": "879a3d4c5682e73f0a3e43c5d91d48a6",
+    "url": "/trekmate/static/media/features-split-image-03.879a3d4c.png"
+  },
+  {
+    "revision": "4d415440e669095c7d0d5778f9162c08",
+    "url": "/trekmate/static/media/features-split-image-04.4d415440.png"
+  },
+  {
+    "revision": "afb0c6cddf65f555be2e477faf37235d",
+    "url": "/trekmate/static/media/features-split-image-05.afb0c6cd.png"
+  },
+  {
+    "revision": "e49627fb5e979e023350423bd7a4a3ca",
+    "url": "/trekmate/static/media/features-split-image-06.e49627fb.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/trekmate/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/trekmate/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/trekmate/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/trekmate/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/trekmate/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/trekmate/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/trekmate/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/trekmate/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/trekmate/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/trekmate/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/trekmate/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/trekmate/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/trekmate/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "58eef92b4e8e5938c0231b41151aef9f",
+    "url": "/trekmate/static/media/trekmate.58eef92b.jpg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/trekmate/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
